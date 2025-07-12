3828421b67def998bdec8ffb47423b0000e153e9
KaueSobreira <sobreirak29@gmail.com>
2025-07-12 17:00:32 -0300

    fix: corrigindo footer em todas as telas não estar fixado no fim da pagina visivel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} flex min-h-screen flex-col antialiased`}
       >
-        {children}
+        <div className="flex-grow">{children}</div>
         <Footer />
       </body>
     </html>
